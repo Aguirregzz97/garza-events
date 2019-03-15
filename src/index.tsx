eede519654e05eee849f545d0912a2d4d167bb45
aguirregzz97 <aguirregzz97@gmail.com>
2019-03-15 12:01:37 -0600

    Boiler Plate Ready

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import * as React from 'react'
 import { render } from 'react-dom'
 import { HashRouter } from 'react-router-dom'
 import { App } from './components/App'
-import { getCurrentUser } from './shared/getCurrentUser'
-import { history } from './shared/history'
-import { getErrorAsString } from './shared/logging/getErrorAsString'
 import { Log } from './shared/logging/Log'
 
 // Used in index.html
@@ -21,40 +18,4 @@ render((
   </HashRouter>
 ), document.getElementById('root'), () => {
   Log.logger.info('Done rendering!')
-  const currentUser = getCurrentUser()
-  // Si el usuario no esta registrado en la empresa, redirigir
-  if (!currentUser) {
-    Log.logger.warn('Redirecting to another webpage because user is not logged in!')
-    history.push(`/#/changeEmpresa`)
-  }
 })
-
-async function installServiceWorkers() {
-  // Service worker registration
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', async () => {
-      Log.logger.info('Running SW code...')
-      try {
-        await navigator.serviceWorker.register('/service-worker.js')
-        Log.logger.info('SW registered!')
-      } catch (error) {
-        Log.logger.error('SW registration failed: ' + getErrorAsString(error))
-      }
-
-      // If ever in need of disabling all service workers, uncomment this
-      // try {
-      //   const registrations = await navigator.serviceWorker.getRegistrations()
-      //   for (const registration of registrations) {
-      //     registration.unregister()
-      //     Log.logger.info('Service worker unregistered')
-      //   }
-      // } catch (error) {
-      //   Log.logger.error('SW unregistration failed: ' + getErrorAsString(error))
-      // }
-    })
-  } else {
-    Log.logger.warn('Browser does not support service workers')
-  }
-}
-
-installServiceWorkers()
