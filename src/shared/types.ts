a0263470b59a373bd47ea803abe32db0055c7e66
Andres <aguirregzz97@gmail.com>
2019-10-02 01:58:19 -0500

    Add forms model

---
+++ src/shared/types.ts
@@ -0,0 +1,12 @@
+export type FormModel = {
+  clientName: string,
+  clientPhone: string,
+  clientAddress: string,
+  eventDate: string,
+  eventSchedule: string,
+  danceFloor: string,
+  loungeEquipment: boolean,
+  ilumination: string,
+  instalationSchedule: string,
+  specialNotes: string,
+}
